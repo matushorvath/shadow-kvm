30eef57f8471b15952871aeda6e29e7f32cde87e
Matus Horvath <matus.horvath@sap.com>
2025-02-23 03:57:51 +0100

    Taskbar icon prototype

---
+++ ShadowKVM/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using H.NotifyIcon;
+
+namespace ShadowKVM;
+
+public partial class App : Application
+{
+    private TaskbarIcon? _notifyIcon;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        _notifyIcon.ForceCreate();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _notifyIcon?.Dispose();
+        base.OnExit(e);
+    }
+}
--- TODO.md
+++ TODO.md
@@ -9,3 +9,11 @@ config UI
 ci cd, releases
 configure device class to look for (keyboard or mouse or GUID?)
 https://github.com/HavenDV/H.NotifyIcon/
+
+
+document icons source and license
+https://icon-icons.com/icon/shadow/264794
+https://icon-icons.com/icon/shadow/264912
+https://icon-icons.com/icon/off-shadow/211187
+https://icon-icons.com/icon/shadow/211348
++ invert
