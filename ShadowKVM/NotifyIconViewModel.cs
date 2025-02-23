aa9d97b3674890a9dd711ddfe9e86cb9be4be8a3
Matus Horvath <matus.horvath@sap.com>
2025-02-23 04:11:00 +0100

    Tray icon basic functionality works

---
+++ ShadowKVM/NotifyIconViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using H.NotifyIcon;
+
+namespace ShadowKVM;
+
+public partial class NotifyIconViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfigureCommand))]
+    public bool canExecuteConfigure = true;
+
+    [RelayCommand(CanExecute = nameof(CanExecuteConfigure))]
+    public void Configure()
+    {
+        Application.Current.MainWindow ??= new MainWindow();
+        Application.Current.MainWindow.Show(disableEfficiencyMode: true);
+        CanExecuteConfigure = false;
+        // TODO set CanExecuteConfigure to true once configuration is closed
+    }
+
+    [RelayCommand]
+    public void Exit()
+    {
+        Application.Current.Shutdown();
+    }
+}
