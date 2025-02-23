09d86906ff743f34f985173dd01b0951392eb1ea
Matus Horvath <matus.horvath@sap.com>
2025-02-23 16:31:29 +0100

    Merge UI code with the console application

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,19 +5,25 @@ namespace ShadowKVM;
 
 public partial class App : Application
 {
-    private TaskbarIcon? _notifyIcon;
-
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
 
         _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
         _notifyIcon.ForceCreate();
+
+        _backgroundTask = new BackgroundTask();
+        _backgroundTask.Start();
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
+        _backgroundTask?.Dispose();
         _notifyIcon?.Dispose();
+
         base.OnExit(e);
     }
+
+    TaskbarIcon? _notifyIcon;
+    BackgroundTask? _backgroundTask;
 }
--- TODO.md
+++ TODO.md
@@ -8,3 +8,12 @@ resources in exe, including icon
 config UI
 ci cd, releases
 configure device class to look for (keyboard or mouse or GUID?)
+
+https://github.com/HavenDV/H.NotifyIcon/tree/master/src/apps/H.NotifyIcon.Apps.Wpf
+
+document icons source and license
+https://icon-icons.com/icon/shadow/264794
+https://icon-icons.com/icon/shadow/264912
+https://icon-icons.com/icon/off-shadow/211187
+https://icon-icons.com/icon/shadow/211348
++ invert
