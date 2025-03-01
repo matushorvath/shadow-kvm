bede9a7cf6a1cefc14e0db3411581830d47a91bb
Matus Horvath <matus.horvath@sap.com>
2025-03-01 17:48:33 +0100

    Update log levels

--- ShadowKVM/BackgroundTask.cs
+++ ShadowKVM/BackgroundTask.cs
@@ -74,7 +74,7 @@ internal class BackgroundTask(Config config) : IDisposable
                 if (matchingMonitors.Count() == 0)
                 {
                     Log.Warning("Did not find any monitors for config {@MonitorConfig}", monitorConfig);
-                    Log.Debug("Following monitors exist: {@Monitors}", monitors);
+                    Log.Information("Following monitors exist: {@Monitors}", monitors);
 
                     continue;
                 }
@@ -82,7 +82,8 @@ internal class BackgroundTask(Config config) : IDisposable
                 foreach (var matchingMonitor in matchingMonitors)
                 {
                     PInvoke.SetVCPFeature(matchingMonitor.Handle, actionConfig.Code, actionConfig.Value);
-                    Log.Debug("Executed action, code 0x{Code:X} value 0x{Value:X}", actionConfig.Code, actionConfig.Value);
+                    Log.Information("Executed action, code 0x{Code:X} value 0x{Value:X} monitor {@Monitor}",
+                        actionConfig.Code, actionConfig.Value, matchingMonitor);
                 }
             }
         }
