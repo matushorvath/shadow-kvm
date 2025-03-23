fa3b24acfc67bd98edd2930c2f7a5daa94e7f3e0
Matus Horvath <matus.horvath@sap.com>
2025-03-24 00:51:40 +0100

    Infrastructure for testing BackgroundTask

--- ShadowKVM.Tests/Devices/BackgroundTask.cs
+++ ShadowKVM.Tests/Devices/BackgroundTask.cs
@@ -26,7 +26,7 @@ public class BackgroundTaskTests
 
     Guid _testGuid = new("{3f527904-28d8-4cda-b1c3-08cca9dc3dff}");
 
-    (Mock<IDeviceNotification>, Channel<IDeviceNotification.Action>) SetupNotification()
+    Mock<IDeviceNotification> SetupNotification(Channel<IDeviceNotification.Action> channel)
     {
         // Registering the notification will return a mock notification
         var notificationMock = new Mock<IDeviceNotification>();
@@ -36,21 +36,20 @@ public class BackgroundTaskTests
             .Returns(notificationMock.Object)
             .Verifiable();
 
-        // The mock notification will pass data to this channe
-        var channel = Channel.CreateUnbounded<IDeviceNotification.Action>();
-
         notificationMock
             .Setup(m => m.Reader)
             .Returns(channel.Reader)
             .Verifiable();
 
-        return (notificationMock, channel);
+        return notificationMock;
     }
 
     [Fact]
     public void Restart_WorksFirstTime()
     {
-        var (notificationMock, channel) = SetupNotification();
+        // The mock notification will pass data to this channel
+        var channel = Channel.CreateUnbounded<IDeviceNotification.Action>();
+        var notificationMock = SetupNotification(channel);
 
         // The task will be restarted with this config
         var config = new Config { TriggerDevice = new() { Raw = _testGuid } };
@@ -75,7 +74,9 @@ public class BackgroundTaskTests
     [Fact]
     public void Restart_WorksSecondTime()
     {
-        var (notificationMock, channel) = SetupNotification();
+        // The mock notification will pass data to this channel
+        var channel = Channel.CreateUnbounded<IDeviceNotification.Action>();
+        var notificationMock = SetupNotification(channel);
 
         // The task will be restarted with this config
         var config = new Config { TriggerDevice = new() { Raw = _testGuid } };
@@ -102,77 +103,106 @@ public class BackgroundTaskTests
         notificationMock.Verify();
     }
 
-    // setup using handle as key [handle] = { monitorconfig, monitordevice }
-
-    [Fact]
-    public void ProcessNotification_OneMonitor()
+    class SetVCPFeatureInvocation
     {
-        var (notificationMock, channel) = SetupNotification();
+        public required byte Code { get; set; }
+        public required uint Value { get; set; }
+    }
 
+    void SetupForProcessNotification(
+        Monitors monitorDevices,
+        IDictionary<nint, SetVCPFeatureInvocation> expectedInvocations,
+        CountdownEvent setVcpFeatureCalled)
+    {
         _monitorServiceMock
             .Setup(m => m.LoadMonitors())
-            .Returns(new Monitors() {
-                new()
-                {
-                    Description = "dEsCrIpTiOn 1",
-                    Handle = new SafePhysicalMonitorHandle(_windowsAPIMock.Object, (HANDLE)12345u, false)
-                },
-                new()
-                {
-                    Description = "dEsCrIpTiOn 2",
-                    Handle = new SafePhysicalMonitorHandle(_windowsAPIMock.Object, (HANDLE)23456u, false)
-                }
-            })
+            .Returns(monitorDevices)
             .Verifiable();
 
-        // The task will be restarted with this config
-        var config = new Config
-        {
-            TriggerDevice = new() { Raw = _testGuid },
-            Monitors = new()
-            {
-                new()
-                {
-                    Description = "dEsCrIpTiOn 1",
-                    Attach = new () { Code = new(0x42), Value = new (0x98) }
-                },
-                new()
-                {
-                    Description = "dEsCrIpTiOn 2",
-                    Attach = new () { Code = new(0x42), Value = new (0x98) }
-                }
-            }
-        };
-
-        // The task is expected to call SetVCPFeature for each monitor (asynchronously)
-        var setVcpFeatureCalled = new CountdownEvent(2); // TODO use monitors.Count or such
         _windowsAPIMock
             .Setup(m => m.SetVCPFeature(It.IsAny<SafeHandle>(), It.IsAny<byte>(), It.IsAny<uint>()))
             .Returns(
                 (SafeHandle hMonitor, byte bVCPCode, uint dwNewValue) =>
                 {
-                    //Assert.Equal(_testHandle.DangerousGetHandle(), hMonitor.DangerousGetHandle());
-                    Assert.Equal(0x42, bVCPCode);
-                    Assert.Equal(0x98u, dwNewValue);
+                    SetVCPFeatureInvocation? expectedInvocation;
+                    Assert.True(expectedInvocations.TryGetValue(
+                        hMonitor.DangerousGetHandle(), out expectedInvocation));
+
+                    Assert.Equal(expectedInvocation.Code, bVCPCode);
+                    Assert.Equal(expectedInvocation.Value, dwNewValue);
 
                     setVcpFeatureCalled.Signal();
                     return 1;
                 }
             )
             .Verifiable();
+    }
+
+    [Theory, MemberData(nameof(TestDataKeys))]
+    public void ProcessNotification_Succeeds(string testDataKey)
+    {
+        var (monitorDevices, monitorConfigs, action, expectedInvocations) = TestData[testDataKey];
+
+        // The mock notification will pass data to this channel
+        var channel = Channel.CreateUnbounded<IDeviceNotification.Action>();
+        var notificationMock = SetupNotification(channel);
+
+        var setVcpFeatureCalled = new CountdownEvent(expectedInvocations.Count);
+        SetupForProcessNotification(monitorDevices, expectedInvocations, setVcpFeatureCalled);
+
+        var config = new Config
+        {
+            TriggerDevice = new() { Raw = _testGuid },
+            Monitors = monitorConfigs.ToList()
+        };
 
         var backgroundTask = new BackgroundTask(_deviceNotificationServiceMock.Object,
             _monitorServiceMock.Object, _windowsAPIMock.Object, _loggerMock.Object);
         backgroundTask.Restart(config);
 
         // Task is now running, send it an action
-        channel.Writer.TryWrite(IDeviceNotification.Action.Arrival);
+        channel.Writer.TryWrite(action);
 
-        // Wait for the background task to call SetVCPFeature
-        setVcpFeatureCalled.Wait();
+        // Wait for the background task to make all calls to SetVCPFeature
+        Assert.True(setVcpFeatureCalled.Wait(TimeSpan.FromSeconds(5)));
 
         _monitorServiceMock.Verify();
         _deviceNotificationServiceMock.Verify();
         notificationMock.Verify();
     }
+
+    static Dictionary<string, (
+        Monitors monitorDevices,
+        MonitorConfig[] monitorConfigs,
+        IDeviceNotification.Action action,
+        Dictionary<nint, SetVCPFeatureInvocation> expectedInvocations
+            )> TestData => new()
+    {
+        ["one monitor"] = new()
+        {
+            monitorDevices = new()
+            {
+                new()
+                {
+                    Description = "dEsCrIpTiOn 1",
+                    Handle = new SafePhysicalMonitorHandle(null!, (HANDLE)12345u, false)
+                }
+            },
+            monitorConfigs =
+            [
+                new()
+                {
+                    Description = "dEsCrIpTiOn 1",
+                    Attach = new () { Code = new(0x42), Value = new (0x98) }
+                }
+            ],
+            action = IDeviceNotification.Action.Arrival,
+            expectedInvocations = new Dictionary<nint, SetVCPFeatureInvocation>
+            {
+                [12345] = new() { Code = 0x42, Value = 0x98 }
+            }
+        }
+    };
+
+    public static TheoryData<string> TestDataKeys => [.. TestData.Keys.AsEnumerable()];
 }
