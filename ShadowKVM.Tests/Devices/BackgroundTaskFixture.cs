5f07d471cda8439d6c228c628f1ec3b994c38b9e
Matus Horvath <matus.horvath@sap.com>
2025-03-27 13:35:09 +0100

    Support multiple input data sets in BackgroundTask test

--- ShadowKVM.Tests/Devices/BackgroundTaskFixture.cs
+++ ShadowKVM.Tests/Devices/BackgroundTaskFixture.cs
@@ -2,6 +2,7 @@ using Moq;
 using Serilog;
 using System.Runtime.InteropServices;
 using System.Threading.Channels;
+using Windows.Win32.Foundation;
 
 namespace ShadowKVM.Tests;
 
@@ -75,5 +76,54 @@ public class BackgroundTaskFixture
         _loggerMock
             .Setup(m => m.Debug("Device notification processed"))
             .Callback(() => finishedEvent.Set());
+    }
+
+    protected static SafePhysicalMonitorHandle H(nuint value)
+        => new SafePhysicalMonitorHandle(null!, (HANDLE)value, false);
+
+    protected record TestDatum(
+        Monitors monitorDevices,
+        MonitorConfig[]? monitorConfigs,
+        IDeviceNotification.Action action,
+        Dictionary<nint, List<SetVCPFeatureInvocation>> invocations);
+
+    protected void TestOneNotification(Dictionary<string, TestDatum> testData, string testDataKey)
+    {
+        var (monitorDevices, monitorConfigs, action, invocations) = testData[testDataKey];
+
+        // The mock notification will pass data to this channel
+        var channel = Channel.CreateUnbounded<IDeviceNotification.Action>();
+        var notificationMock = SetupNotification(channel);
+
+        var finishedEvent = new AutoResetEvent(false);
+        SetupForProcessOneNotification(monitorDevices, invocations, finishedEvent);
+
+        var config = new Config
+        {
+            TriggerDevice = new() { Raw = _testGuid },
+            Monitors = monitorConfigs?.ToList()
+        };
+
+        using (var backgroundTask = new BackgroundTask(_deviceNotificationServiceMock.Object,
+            _monitorServiceMock.Object, _windowsAPIMock.Object, _loggerMock.Object))
+        {
+            backgroundTask.Restart(config);
+
+            // Task is now running, send it an action
+            channel.Writer.TryWrite(action);
+
+            // Wait for the background task to make all calls to SetVCPFeature
+            Assert.True(finishedEvent.WaitOne(TimeSpan.FromSeconds(5)));
+        }
+
+        // Check no other SetVCPFeature calls were made
+        _windowsAPIMock
+            .Verify(
+                m => m.SetVCPFeature(It.IsAny<SafeHandle>(), It.IsAny<byte>(), It.IsAny<uint>()),
+                Times.Exactly(invocations.Sum(i => i.Value.Count)));
+
+        _monitorServiceMock.Verify();
+        _deviceNotificationServiceMock.Verify();
+        notificationMock.Verify();
     }
 }
